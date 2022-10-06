eb9ea94a86bf9ad675abfca50e3c02069d496e69
JaqKent <jaq.kent@gmail.com>
2022-10-06 17:35:09 -0300

    slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+/* import Slider from 'components/Slider'; */
+import Slider from 'components/Slider';
+import { ITEMS } from 'constants/constants';
 import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Slider slides={ITEMS} />
       <h1>mundo regalo</h1>
     </div>
   );
---
+++ src/constants/constants.ts
@@ -0,0 +1,18 @@
+import item1 from 'assets/articulo1.jpg';
+import item2 from 'assets/articulo2.jpg';
+import item3 from 'assets/articulo3.jpg';
+
+export const ITEMS = [
+  {
+    id: 1,
+    img: item1,
+  },
+  {
+    id: 2,
+    img: item2,
+  },
+  {
+    id: 3,
+    img: item3,
+  },
+];
