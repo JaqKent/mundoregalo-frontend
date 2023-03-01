1782068fe1ff2a8b756278bdd459ae8ac78bd05e
JaqKent <jaq.kent@gmail.com>
2023-03-01 12:07:49 -0300

    mr-52-product card vertical landing page

--- src/components/CustomCard/index.tsx
+++ src/components/CustomCard/index.tsx
@@ -1,3 +1,5 @@
+import { faDollarSign, faStar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './styles.module.scss';
 
 interface Props {
@@ -5,11 +7,14 @@ interface Props {
     name:string;
     price:number;
     small?:boolean;
+    stars:number,
+    votes:number,
+    info?:boolean,
 
 }
 
 function CustomCard({
-  image, name, price, small,
+  image, name, price, small,stars,votes,info
 } : Props) {
   return (
     <div className={small ? styles.containerSmall : styles.containerLarge}>
@@ -23,11 +28,11 @@ function CustomCard({
           </span>
         </div>
         <div className={`${small ? styles.priceSmall : styles.priceLarge}`}>
-          <span>4.8(15999)</span>
-          <span>
-            $
-            {price}
-          </span>
+          <div className={styles.info}><span className={styles.rating}><FontAwesomeIcon icon={faStar}/>{stars}({votes}) </span>
+          <span><FontAwesomeIcon icon={faDollarSign}/>{price}</span></div>
+          <div className={styles.info}><span className={ info ? styles.block : ""}>Onsale</span>
+          <span className={info ? styles.block : ""}>delivery</span></div>
+          
         </div>
       </div>
     </div>
