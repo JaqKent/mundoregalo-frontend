23164d6115e837a614dd6577a0cb827482eca9b5
JaqKent <jaq.kent@gmail.com>
2023-05-23 12:04:31 -0300

    mr-70-product-page-implementation

--- src/components/ProductPage/components/quantitySelector/index.tsx
+++ src/components/ProductPage/components/quantitySelector/index.tsx
@@ -6,28 +6,34 @@ import { getFormattedQuantity } from '~constants/utils';
 
 function QuantitySelector() {
     const [quantity, setQuantity] = useState(0);
-    const add = () => {
-        setQuantity(quantity + 1);
+
+    const updateQuantity = (operation: boolean) => {
+        setQuantity((prevQuantity) =>
+            operation ? prevQuantity + 1 : prevQuantity - 1
+        );
     };
-    const subtract = () => {
-        setQuantity(quantity - 1);
-    };
+
+    const QUANTITY = '993 disponibles';
 
     return (
         <div className={styles.container}>
             <div className={styles.stock}>
                 Cantidad
-                <span className={styles.spanStock}>(993 disponibles)</span>
+                <span className={styles.spanStock}>({QUANTITY})</span>
             </div>
             <div className={styles.selector}>
-                <button type="button" onClick={add} className={styles.button}>
+                <button
+                    type="button"
+                    onClick={() => updateQuantity(true)}
+                    className={styles.button}
+                >
                     +
                 </button>
                 <p>{getFormattedQuantity(quantity)}</p>
 
                 <button
                     type="button"
-                    onClick={subtract}
+                    onClick={() => updateQuantity(false)}
                     className={styles.button}
                 >
                     -
