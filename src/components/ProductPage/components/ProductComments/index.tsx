b9ce99945e62265f5ddb0768d082d740303a73db
JaqKent <jaq.kent@gmail.com>
2023-06-12 21:34:29 -0300

    mr-72-ProductPage-relatedProduct-and-comments

--- src/components/ProductPage/components/ProductComments/index.tsx
+++ src/components/ProductPage/components/ProductComments/index.tsx
@@ -1,10 +1,64 @@
+import { useState } from 'react';
+import { faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import StarRating from '../StarRating';
+
+import { COMMENTS } from './constant';
+
 import styles from './styles.module.scss';
 
 function ProductComments() {
     return (
         <div className={styles.container}>
-            <div className={styles.stars}>strellas</div>
-            <div>comentario box</div>
+            <div className={styles.title}>
+                <p>Opiniones de nuestros clientes</p>
+                <div className={styles.stars}>
+                    <StarRating initialValue={4} readonly size={19.95} />
+                    <span className={styles.rating}>51.215</span>
+                </div>
+            </div>
+            <div className={styles.commentbox}>
+                {COMMENTS.map((item) => (
+                    <div key={item.id}>
+                        <div className={styles.user}>
+                            <img
+                                src={item.img}
+                                alt={item.user}
+                                className={styles.img}
+                            />
+                            <p className={styles.username}>{item.user}</p>
+                        </div>
+                        <div className={styles.commentText}>{item.comment}</div>
+                        <div className={styles.like}>
+                            <div className={styles.help}>
+                                <p>
+                                    A {item.help} personas le ayudo esta opinion
+                                </p>
+                                <FontAwesomeIcon
+                                    icon={faThumbsUp}
+                                    className={
+                                        item.like
+                                            ? styles.thumbsUpClicked
+                                            : styles.thumbsUp
+                                    }
+                                />
+                            </div>
+                            <div className={styles.dislike}>
+                                <FontAwesomeIcon
+                                    icon={faThumbsDown}
+                                    flip="horizontal"
+                                    className={
+                                        item.like
+                                            ? styles.thumbsDown
+                                            : styles.thumbsDownClicked
+                                    }
+                                />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
--- src/components/ProductPage/index.tsx
+++ src/components/ProductPage/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+import ProductComments from './components/ProductComments';
 import QuantitySelector from './components/QuantitySelector';
 import SelectDropdown from './components/SelectDropdown';
 import StarRating from './components/StarRating';
@@ -17,64 +18,68 @@ function ProductPage() {
     const handleBuyNow = () => console.log('buy now');
 
     return (
-        <div className={styles.container}>
-            <div className={styles.title}>
-                <div className={styles.text}>
-                    Text wieght 400 align left poppin Text wieght 400 align left
-                    poppinText
+        <>
+            <div className={styles.container}>
+                <div className={styles.title}>
+                    <div className={styles.text}>
+                        Text wieght 400 align left poppin Text wieght 400 align
+                        left poppinText
+                    </div>
+                    <div className={styles.stars}>
+                        <StarRating initialValue={4} readonly size={11.38} />
+                        <span className={styles.rating}>51.215</span>
+                    </div>
                 </div>
-                <div className={styles.stars}>
-                    <StarRating initialValue={3} readonly size={11.38} />
-                    <span className={styles.rating}>51.215</span>
+                <div className={styles.slider}>
+                    <Slider
+                        hide
+                        auto={auto}
+                        setAuto={setAuto}
+                        slides={ITEMS}
+                        className={styles.image}
+                        minClassName={styles.miniatures}
+                        mini={styles.mini}
+                        miniActive={styles.miniActive}
+                    />
                 </div>
-            </div>
-            <div className={styles.slider}>
-                <Slider
-                    hide
-                    auto={auto}
-                    setAuto={setAuto}
-                    slides={ITEMS}
-                    className={styles.image}
-                    minClassName={styles.miniatures}
-                    mini={styles.mini}
-                    miniActive={styles.miniActive}
-                />
-            </div>
 
-            <div>
-                <SelectDropdown />
-            </div>
-            <div className={styles.price}>
-                <div className={styles.priceNumber}>
-                    <h4>Precio:</h4>
-                    <h3>$56.094</h3>
-                </div>
-                <div className={styles.delivery}>
-                    <h4>Envio: </h4>
-                    <p>Gratis</p>
-                </div>
-            </div>
-            <div>
-                <QuantitySelector />
-            </div>
-            <div className={styles.buttons}>
                 <div>
-                    <CustomButton
-                        className={styles.buttonCart}
-                        label="Agregar al Carrito"
-                        onClick={handleAddToCart}
-                    />
+                    <SelectDropdown />
+                </div>
+                <div className={styles.price}>
+                    <div className={styles.priceNumber}>
+                        <h4>Precio:</h4>
+                        <h3>$56.094</h3>
+                    </div>
+                    <div className={styles.delivery}>
+                        <h4>Envio: </h4>
+                        <p>Gratis</p>
+                    </div>
                 </div>
                 <div>
-                    <CustomButton
-                        className={styles.buttonNow}
-                        label="Comprar ahora"
-                        secondary
-                        onClick={handleBuyNow}
-                    />
+                    <QuantitySelector />
+                </div>
+                <div className={styles.buttons}>
+                    <div>
+                        <CustomButton
+                            className={styles.buttonCart}
+                            label="Agregar al Carrito"
+                            onClick={handleAddToCart}
+                        />
+                    </div>
+                    <div>
+                        <CustomButton
+                            className={styles.buttonNow}
+                            label="Comprar ahora"
+                            secondary
+                            onClick={handleBuyNow}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
+            <ProductComments />
+            <div />
+        </>
     );
 }
 
