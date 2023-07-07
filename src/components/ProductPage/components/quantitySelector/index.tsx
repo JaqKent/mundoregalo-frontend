18c264f54b7cc09f33877ff714230a4cf66146d9
JaqKent <jaq.kent@gmail.com>
2023-07-07 12:32:10 -0300

    mr-#73/76/77 quantity input- invert order of buttons- adjust negative values& put the limit in the total stock

--- src/components/ProductPage/components/quantitySelector/index.tsx
+++ src/components/ProductPage/components/quantitySelector/index.tsx
@@ -1,42 +1,85 @@
-import { useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 
 import styles from './styles.module.scss';
 
 import { getFormattedQuantity } from '~constants/utils';
 
+const QUANTITY = 993;
+
 function QuantitySelector() {
     const [quantity, setQuantity] = useState(0);
+    const [customQuantity, setCustomQuantity] = useState(quantity.toString());
 
-    const updateQuantity = (operation: boolean) => {
-        setQuantity((prevQuantity) =>
-            operation ? prevQuantity + 1 : prevQuantity - 1
-        );
+    const updateQuantity = (operation: boolean, value?: string) => {
+        if (value) {
+            const newValue = parseInt(value, 10);
+            if (!Number.isNaN(newValue)) {
+                const newQuantity = Math.min(newValue, QUANTITY);
+                setQuantity(newQuantity);
+                setCustomQuantity(newValue.toString());
+            }
+        } else {
+            setQuantity((prevQuantity) =>
+                operation && prevQuantity < QUANTITY
+                    ? prevQuantity + 1
+                    : prevQuantity - 1
+            );
+            setCustomQuantity((prevQuantity) =>
+                operation && parseInt(prevQuantity, 10) < QUANTITY
+                    ? (parseInt(prevQuantity, 10) + 1).toString()
+                    : (parseInt(prevQuantity, 10) - 1).toString()
+            );
+        }
     };
 
-    const QUANTITY = '993 disponibles';
+    const handleCustomQuantityChange = (
+        event: ChangeEvent<HTMLInputElement>
+    ) => {
+        const { value } = event.target;
+        if (/^[0-9]*$/.test(value)) {
+            const newQuantity = Math.min(parseInt(value, 10), QUANTITY);
+            setCustomQuantity(newQuantity.toString());
+        }
+    };
+
+    const handleCustomQuantityBlur = () => {
+        if (customQuantity.trim() === '') {
+            setCustomQuantity(quantity.toString());
+        } else {
+            updateQuantity(false, customQuantity);
+        }
+    };
 
     return (
         <div className={styles.container}>
             <div className={styles.stock}>
                 Cantidad
-                <span className={styles.spanStock}>({QUANTITY})</span>
+                <span className={styles.spanStock}>
+                    ({QUANTITY} disponibles)
+                </span>
             </div>
             <div className={styles.selector}>
+                <button
+                    disabled={quantity <= 0}
+                    type="button"
+                    onClick={() => updateQuantity(false)}
+                    className={styles.button}
+                >
+                    -
+                </button>
+                <input
+                    type="text"
+                    value={getFormattedQuantity(customQuantity)}
+                    onChange={handleCustomQuantityChange}
+                    onBlur={handleCustomQuantityBlur}
+                    className={styles.input}
+                />
                 <button
                     type="button"
                     onClick={() => updateQuantity(true)}
                     className={styles.button}
                 >
                     +
-                </button>
-                <p>{getFormattedQuantity(quantity)}</p>
-
-                <button
-                    type="button"
-                    onClick={() => updateQuantity(false)}
-                    className={styles.button}
-                >
-                    -
                 </button>
             </div>
         </div>
