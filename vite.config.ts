8a5f25431692360e2326079af1de9e8bf55ed714
Aldo Exequiel Mleziva <aldo.exequiel.mleziva@hp.com>
2023-12-01 15:39:51 -0300

    chore: Updated yarn lock and configs in tsconfig y vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,58 @@
-import { defineConfig } from 'vite';
+/* eslint-disable import/no-extraneous-dependencies */
+
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: [
-            {
-                find: '~components',
-                replacement: path.resolve(__dirname, 'src/components'),
-            },
-            {
-                find: '~context',
-                replacement: path.resolve(__dirname, 'src/context'),
-            },
-            {
-                find: '~assets',
-                replacement: path.resolve(__dirname, 'src/assets'),
-            },
-            {
-                find: '~scss',
-                replacement: path.resolve(__dirname, 'src/scss'),
-            },
-            {
-                find: '~screens',
-                replacement: path.resolve(__dirname, 'src/screens'),
-            },
-            {
-                find: '~hooks',
-                replacement: path.resolve(__dirname, 'src/hooks'),
-            },
-            {
-                find: '~interfaces',
-                replacement: path.resolve(__dirname, 'src/interfaces'),
-            },
-            {
-                find: '~constants',
-                replacement: path.resolve(__dirname, 'src/constants'),
-            },
-            {
-                find: '~services',
-                replacement: path.resolve(__dirname, 'src/services'),
-            },
-            {
-                find: '~configs',
-                replacement: path.resolve(__dirname, 'src/configs'),
-            },
-            {
-                find: '~utils',
-                replacement: path.resolve(__dirname, 'src/utils'),
-            },
-        ],
-    },
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '~components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      {
+        find: '~context',
+        replacement: path.resolve(__dirname, 'src/context'),
+      },
+      {
+        find: '~assets',
+        replacement: path.resolve(__dirname, 'src/assets'),
+      },
+      {
+        find: '~scss',
+        replacement: path.resolve(__dirname, 'src/scss'),
+      },
+      {
+        find: '~screens',
+        replacement: path.resolve(__dirname, 'src/screens'),
+      },
+      {
+        find: '~hooks',
+        replacement: path.resolve(__dirname, 'src/hooks'),
+      },
+      {
+        find: '~interfaces',
+        replacement: path.resolve(__dirname, 'src/interfaces'),
+      },
+      {
+        find: '~constants',
+        replacement: path.resolve(__dirname, 'src/constants'),
+      },
+      {
+        find: '~services',
+        replacement: path.resolve(__dirname, 'src/services'),
+      },
+      {
+        find: '~configs',
+        replacement: path.resolve(__dirname, 'src/configs'),
+      },
+      {
+        find: '~utils',
+        replacement: path.resolve(__dirname, 'src/utils'),
+      },
+    ],
+  },
 });
