2beca91bce0590589607c44302236ba6035dbc57
Luis Ramirez <jaq.kent@gmail.com>
2023-12-11 21:42:07 -0300

    mr-##-update

--- src/App/NotFound.tsx
+++ src/App/NotFound.tsx
@@ -1,14 +1,7 @@
-import { useRouteError } from 'react-router-dom';
-
 function NotFound() {
-  const error = useRouteError();
-
   return (
     <div>
       <h1>La pagina solicitada no existe</h1>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
     </div>
   );
 }
